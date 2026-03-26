6eaac9e1ce61a8f85f7ac697a7be20317762523b
Moran Abilea <moran.abilea@ibm.com>
2026-03-26 14:16:46 +0000

    fix clear review notes
    Signed-off-by: Moran Abilea <moran.abilea@ibm.com>

--- node/examples/all-in-one/scripts/clean_sample.sh
+++ node/examples/all-in-one/scripts/clean_sample.sh
@@ -7,6 +7,8 @@
 
 set -eux
 
+BASE=/tmp/arma-all-in-one
+
 echo "Stopping all-in-one container..."
 
 docker ps -q --filter "ancestor=arma-4p1s" | xargs -r docker stop
@@ -15,8 +17,17 @@ echo "Removing container..."
 
 docker ps -aq --filter "ancestor=arma-4p1s" | xargs -r docker rm
 
+echo "Fixing permissions..."
+
+if [ -d "${BASE}" ]; then
+  docker run --rm \
+    -v ${BASE}:${BASE} \
+    alpine \
+    sh -c "chmod -R u+w ${BASE}"
+fi
+
 echo "Removing storage..."
 
-rm -rf /tmp/arma-all-in-one
+rm -rf ${BASE}
 
 echo "Clean done."
